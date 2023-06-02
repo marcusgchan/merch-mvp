f23bcf5851426f06eed9c196c980f81523d54bf0
Marcus <marcuscchan@gmail.com>
2023-06-01 17:53:04 -0700

    Added tables and routes

---
+++ src/pages/products/[[...index]].tsx
@@ -0,0 +1,18 @@
+export default function Products() {
+  return (
+    <main>
+      <h1>Products</h1>
+      <section></section>
+    </main>
+  );
+}
+
+function Product() {
+  return (
+    <button>
+      <div>image here</div>
+      <h2>Product</h2>
+      <span>Price</span>
+    </button>
+  );
+}
